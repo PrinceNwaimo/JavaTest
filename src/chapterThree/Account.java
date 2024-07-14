b3b493c98acabc6c6bebbf85b734d398e14a336a
Prince Nwaimo <nwaimoprince12@gmail.com>
2024-07-14 04:52:08 +0100

    dosage one

--- src/chapterThree/Account.java
+++ src/chapterThree/Account.java
@@ -2,11 +2,30 @@ package chapterThree;
 
 public class Account {
     private String name;
+    private double balance;
+
+    public Account (String name, double balance1){
+        this.name = name;
+        if(balance1 > 0.0)
+        balance = balance1;
+
+    }
 
     public void setName(String name){
         this.name = name;
     }
     public String getName(){
         return name;
+    }
+    public void setBalance(double balance1){
+        balance = balance1;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void deposit(double depositAmount){
+        if(depositAmount > 0.0){
+            balance = balance + depositAmount;
+        }
     }
 }
--- src/chapterThree/AccountTest.java
+++ src/chapterThree/AccountTest.java
@@ -6,12 +6,18 @@ public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Account myAccount = new Account();
-        System.out.printf("Initial Name is : %s%n%n", myAccount.getName());
+        Account myAccount = new Account("Fidelity Bank", 1000);
+        Account account1 = new Account("Diamond Bank", 0.0);
+        System.out.printf("Initial Name is : %s%n%n", myAccount.getName(),myAccount.getBalance());
+        System.out.printf("Initial Name is : %s%n%n", account1.getName(), myAccount.getBalance());
         System.out.println("Please  enter the name: ");
         String newName = input.nextLine();
         myAccount.setName(newName);
+        System.out.println("Name of Second Account: ");
+        String newName1 = input.nextLine();
+        account1.setName(newName1);
         System.out.println();
-        System.out.println("Name in object myAccount is:"+ myAccount.getName());
+        System.out.println("Name in object myAccount is: " + myAccount.getName()+","+myAccount.getBalance());
+        System.out.println("Name of account 1 holder is now: "+account1.getName()+","+account1.getBalance());
     }
 }
