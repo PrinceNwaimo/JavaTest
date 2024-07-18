cc8258dc2478b12dbc5b7e59c883409abf22fef1
Prince Nwaimo <nwaimoprince12@gmail.com>
2024-07-18 11:19:24 +0100

    second commit

--- src/GetterAndSetterPractice/HeartRateTest.java
+++ src/GetterAndSetterPractice/HeartRateTest.java
@@ -6,6 +6,8 @@ public class HeartRateTest {
     public static void main(String[] args) {
         HeartRates cardio = new HeartRates("FirstName","LastName","Date of Birth");
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first name here: ");
+
 
     }
 }
--- src/GetterAndSetterPractice/HeartRates.java
+++ src/GetterAndSetterPractice/HeartRates.java
@@ -9,6 +9,10 @@ public class HeartRates {
     private String lastName;
     private String dateOfBirth;
 
+    private int age;
+
+    private int HR;
+
     public HeartRates(String firstName, String lastName, String dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -42,8 +46,17 @@ public class HeartRates {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate birthdate = LocalDate.parse(dob,formatter);
         LocalDate currentDate = LocalDate.now();
-        int age = Period.between(LocalDate.parse(dob),currentDate).getYears();
+        age = Period.between(LocalDate.parse(dob),currentDate).getYears();
         return age;
     }
+    public int MaxHeartRate(){
+         HR = 220 - age;
+        return HR;
+    }
+    public int TargetHR(double range){
+        int target = (int) (range * HR);
+        return target;
+    }
+
 
 }
