9bbede00684237eb376b81feba1f3ec7f16b2a67
Prince Nwaimo <nwaimoprince12@gmail.com>
2024-08-06 22:39:13 +0100

    late

--- src/chapterThree/SortingIntegers.java
+++ src/chapterThree/SortingIntegers.java
@@ -6,10 +6,26 @@ public class SortingIntegers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter three integers: ");
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int c = input.nextInt();
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
 
+        int temp;
+        if (num1 > num2){
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
 
+        if (num1 > num3){
+            temp = num1;
+            num1 = num3;
+            num3 = temp;
+        }
+        if (num2 > num3){
+            temp = num2;
+            num2 = num3;
+            num3 = temp;
+        }
     }
 }
