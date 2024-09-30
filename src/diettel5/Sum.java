32e393a5d82fab7eec26fd26049538874fa48bc5
Prince Nwaimo <nwaimoprince12@gmail.com>
2024-09-30 06:27:33 +0100

    Monday

---
+++ src/diettel5/Sum.java
@@ -0,0 +1,11 @@
+package diettel5;
+
+public class Sum {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int number = 2; number <= 20; number += 2){
+            total += number;
+        }
+        System.out.printf("sum is %d%n", total);
+    }
+}
