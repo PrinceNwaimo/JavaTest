8d28f3b054e2bece3f6c0ec891c207f6145d8c57
Prince Nwaimo <nwaimoprince12@gmail.com>
2024-10-15 06:38:12 +0100

    lovely

--- src/diettel5/ModifiedInterest.java
+++ src/diettel5/ModifiedInterest.java
@@ -3,17 +3,15 @@ package diettel5;
 public class ModifiedInterest {
     public static void main(String[] args) {
         double principal = 1000.0;
-        double rate = 0.05;
+        System.out.printf("%s%20s%20s%n", "Year", "Rate", "Amount on deposit");
+        for (double rate = 0.05; rate <= 0.10; rate += 0.01) {
+            System.out.printf("%nInterest Rate: %.0f%%%n", rate * 100);
 
-        System.out.printf("%s%20s%n", "Year","Amount on deposit");
-        for(int year = 1; year <= 10; ++year){
-            for ( double rateTrack = rate; rateTrack <=0.10 ; rateTrack++) {
-
-                double amount = principal * Math.pow(1.0 + rateTrack, year);
-
-                System.out.printf("%4d%,20.2f%n", year, amount);
+            for (int year = 1; year <= 10; ++year) {
+                double amount = principal * Math.pow(1.0 + rate, year);
+                System.out.printf("%4d%,20.2f%,20.2f%%%n", year, amount, rate * 100);
             }
         }
     }
-}
 
+}
