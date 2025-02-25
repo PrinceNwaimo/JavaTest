301924f421e1849abafeb02bd978ec2563488029
Prince Nwaimo <nwaimoprince12@gmail.com>
2025-02-25 09:46:29 +0100

    Idea

---
+++ src/joyceChapter4/BillingTest.java
@@ -0,0 +1,8 @@
+package joyceChapter4;
+
+public class BillingTest {
+    public static void main(String[] args) {
+        Billing cart = new Billing();
+        cart.computeBill(500,20,10);
+    }
+}
