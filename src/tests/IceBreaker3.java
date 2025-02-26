02753986c21d9a45459e415dc43fcac2736359a2
Prince Nwaimo <nwaimoprince12@gmail.com>
2025-02-26 11:35:47 +0100

    ice breaker

--- src/tests/IceBreaker3.java
+++ src/tests/IceBreaker3.java
@@ -1,15 +1,31 @@
 package tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// Question: Write me a function that takes 3 integers (a,b and c), and returns the multiples of the number c between an and b.
+
 public class IceBreaker3 {
     public static void main(String[] args) {
-
+    List<Integer>multiples = findMultiples(5,90,2);
+        System.out.println(multiples);
     }
+    public static List<Integer> findMultiples(int firstNumber, int secondNumber, int thirdNumber) {
+        List<Integer> multiples = new ArrayList<>();
 
-    private static void multiple(int a, int b, int c){
-        for (int i = c; i <b ; i++) {
-            System.out.println();
+        if (firstNumber > secondNumber) {
+            int temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
         }
 
+        for (int i = firstNumber; i <= secondNumber; i++) {
+            if (i % thirdNumber == 0) {
+                multiples.add(i);
+            }
+        }
+
+        return multiples;
     }
 }
 
