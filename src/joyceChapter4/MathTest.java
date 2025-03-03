84ce9dcaa023beb8e494788b61e455422d245fb0
Prince Nwaimo <nwaimoprince12@gmail.com>
2025-03-03 13:20:28 +0100

    flex

---
+++ src/joyceChapter4/MathTest.java
@@ -0,0 +1,10 @@
+package joyceChapter4;
+
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(37));
+        System.out.println(Math.sin(300));
+        System.out.println(Math.cos(300));
+        System.out.println(Math.floor(22.8) +", "+ Math.ceil(22.8)+ ", " +Math.round(22.8));
+    }
+}
