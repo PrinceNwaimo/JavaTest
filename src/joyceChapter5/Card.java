3754249271b45c85f4dc9a18a1773747d1cdd8a8
Prince Nwaimo <nwaimoprince12@gmail.com>
2025-03-20 15:19:31 +0100

    card

---
+++ src/joyceChapter5/Card.java
@@ -0,0 +1,49 @@
+package joyceChapter5;
+
+public class Card {
+    private int value;
+    private String suit;
+
+    public Card(int setValue, String setSuit) {
+        setValue(setValue);
+        suit = setSuit;
+    }
+
+    public void setValue(int setValue) {
+        if (setValue < 1 || setValue > 13) {
+            value = 1;
+        } else {
+            value = setValue;
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String toString() {
+        String valueStr = "";
+        switch (value) {
+            case 1:
+                valueStr = "Ace";
+                break;
+            case 11:
+                valueStr = "Jack";
+                break;
+            case 12:
+                valueStr = "Queen";
+                break;
+            case 13:
+                valueStr = "King";
+                break;
+            default:
+                valueStr = Integer.toString(value);
+        }
+        return valueStr + " of " + suit;
+    }
+
+}
---
+++ src/joyceChapter5/War.java
@@ -0,0 +1,37 @@
+package joyceChapter5;
+
+import java.util.Random;
+
+public class War {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+
+        Card playerCard = new Card(rand.nextInt(13) + 1, getSuit(rand));
+        Card computerCard = new Card(rand.nextInt(13) + 1, getSuit(rand));
+
+        // Ensure the two cards are not the same
+        while (playerCard.getValue() == computerCard.getValue() && playerCard.getSuit().equals(computerCard.getSuit())) {
+            computerCard = new Card(rand.nextInt(13) + 1, getSuit(rand));
+        }
+
+        // Determine the higher card
+        System.out.println("Player's card: " + playerCard);
+        System.out.println("Computer's card: " + computerCard);
+
+        if (playerCard.getValue() > computerCard.getValue()) {
+            System.out.println("Player wins!");
+        } else if (playerCard.getValue() < computerCard.getValue()) {
+            System.out.println("Computer wins!");
+        } else {
+            System.out.println("It's a tie!");
+        }
+    }
+
+
+    public static String getSuit(Random rand) {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        return suits[rand.nextInt(4)];
+}
+
+}
