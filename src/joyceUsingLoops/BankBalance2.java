806655ae2e3d914d59e663662a79bc98f807cb8f
PrinceNwaimo <nwaimoprince12@gmail.com>
2025-04-30 14:44:18 +0100

    left

---
+++ src/joyceUsingLoops/BankBalance2.java
@@ -0,0 +1,26 @@
+package joyceUsingLoops;
+
+import java.util.Scanner;
+
+public class BankBalance2 {
+    public static void main(String[] args) {
+        double balance;
+        int response;
+        int year = 1;
+        final double INT_RATE = 0.05;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter initial bank balance > ");
+        balance = keyboard.nextDouble();
+
+        do {
+            balance = balance + balance * INT_RATE;
+            System.out.println("After year " + year + " at " + INT_RATE + " interest rate, balance is $"+ balance);
+            year = year + 1;
+            System.out.println("\nDo you want to see the balance "+ "at the end of another year?");
+            System.out.println("enter 1 for yes");
+            System.out.print(" or any other number for no >>");
+            response = keyboard.nextInt();
+        }
+        while (response == 1);
+    }
+}
