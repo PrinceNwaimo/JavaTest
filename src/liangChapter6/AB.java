433594fbff6c4dc1ffefdbd398461100e3ba42c8
PrinceNwaimo <nwaimoprince12@gmail.com>
2025-06-13 11:34:19 +0100

    like

---
+++ src/liangChapter6/AB.java
@@ -0,0 +1,10 @@
+package liangChapter6;
+
+public class AB {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i <= 10; i++)
+            sum += i;
+        System.out.println("Sum from 1 to 10 is " + sum);
+    }
+}
