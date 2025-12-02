e45be0cd8ef43002cec8cddcc297b75c589df33e
PrinceNwaimo <nwaimoprince12@gmail.com>
2025-12-02 23:33:52 +0100

    Lively

---
+++ src/networkProgramming/Server.java
@@ -0,0 +1,50 @@
+package networkProgramming;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Server extends Application{
+    @Override
+    public void start(Stage primaryStage){
+        TextArea ta = new TextArea();
+
+        Scene scene = new Scene(new ScrollPane(), 450,200);
+        primaryStage.setTitle("Server");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        new Thread(()-> {
+            try{
+                ServerSocket serverSocket = new ServerSocket(8000);
+                Platform.runLater(()-> ta.appendText("Server started at " + new Date() + '\n'));
+
+                Socket socket = serverSocket.accept();
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+                while(true){
+                    double radius = inputFromClient.readDouble();
+
+                    double area = radius * radius * Math.PI;
+
+                    outputToClient.writeDouble(area);
+
+                    Platform.runLater(()-> { ta.appendText("Radius received from client: " + radius + '\n');
+                    ta.appendText("Area is: "+ area + '\n');});
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }).start();
+    }
+}
